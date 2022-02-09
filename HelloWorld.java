99edd69ab54162d9a3acac1e33ae6433d781aee1
Stubbledorange <ctierney007@gmail.com>
2022-02-08 19:40:53 -0500

    Revert "Added print functions for a name and age"
    This reverts commit 9f0547857f1128d5ce40bddb50871be6ad3678a3.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,8 @@
 //Collin Tierney helloworld
 
-public class HelloWorld 
-{
+public class HelloWorld {
    public static void main(String[] args)
    {
       System.out.println("Hello World");
-      
-      String name = "R Massey";
-      
-      System.out.println(name);
-      
-      int age = 22;
-      
-      System.out.println(age);
-    }// end of main
-}//end of helloWorld
+   }
+   }
